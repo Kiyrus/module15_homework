dfcdc8552a80efb5203d6dd419970fd256366102
Ilya Kosov <kosov.ilya@bk.ru>
2023-06-30 13:44:21 +0300

    Task_3 completed

--- Task_3/task_3.js
+++ Task_3/task_3.js
@@ -14,22 +14,57 @@ webSocket.onopen = function () {
     outputConnectionArea.innerText = "CONNECTED";
 };
 
+webSocket.onerror = function () {
+    output.innerText = "ERROR";
+};
+
+webSocket.onmessage = (event) => {
+    writeServerMessage(event.data);
+};
+
 buttonSendMessage.addEventListener("click", () => {
     let message = inputMessageArea.value;
     if (message !== "") {
         let message = inputMessageArea.value;
-        writeToScreen(message);
+        writeClientMessage(message);
         webSocket.send(message);
         outputChatMessageArea.scrollTop = outputChatMessageArea.scrollHeight;
     } else {
-        alert("Enter message, pls)")
+        alert("Enter message, pls)");
     }
 });
 
-function writeToScreen(message) {
-    let pre = document.createElement("p");
-    pre.className = ("client-message");
-    pre.innerHTML = message;
-    outputChatMessageArea.appendChild(pre);
-    inputMessageArea.value = ("");
+function writeClientMessage(message) {
+    let getMessage = document.createElement("p");
+    getMessage.className = "client-message";
+    getMessage.innerHTML = message;
+    outputChatMessageArea.appendChild(getMessage);
+    inputMessageArea.value = "";
 }
+
+function writeServerMessage(event) {
+    let getServerMessage = document.createElement("p");
+    getServerMessage.className = "server-message";
+    getServerMessage.innerHTML = event;
+    outputChatMessageArea.appendChild(getServerMessage);
+    outputChatMessageArea.scrollTop = outputChatMessageArea.scrollHeight;
+}
+
+function writeGeolocationMessage(latitude, longitude) {
+    let geolocationMessage = document.createElement("a");
+    geolocationMessage.setAttribute("href", `https://www.openstreetmap.org/#map=18/${latitude}/${longitude}`);
+    geolocationMessage.className = "client-message";
+    geolocationMessage.innerHTML = "My geolocation";
+    outputChatMessageArea.appendChild(geolocationMessage);
+}
+
+const successGeolocation = (position) => {
+    const latitude = position.coords.latitude;
+    const longitude = position.coords.longitude;
+    writeGeolocationMessage(latitude, longitude);
+    outputChatMessageArea.scrollTop = outputChatMessageArea.scrollHeight;
+};
+
+buttonGetGeolocation.addEventListener("click", () => {
+    navigator.geolocation.getCurrentPosition(successGeolocation);
+});
